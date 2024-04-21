39329e208b4c2136c22b9ac3bfc6c25cae9a6045
Khant Sithu <khantseithuu@gmail.com>
2024-04-21 09:11:47 +0630

    Add solution for valid binary tree problem

---
+++ is_valid_bst.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, lower=float('-inf'),upper=float('inf')):
+            if not node:
+                return True
+            val = node.val
+            if val <= lower or val >= upper:
+                return False
+            if not helper(node.right, val, upper):
+                return False
+            if not helper(node.left, lower, val):
+                return False
+            return True
+
+        return helper(root)
+
+# Time complexity: O(N)
+# Space complexity: O(H) where h is the height of the tree.
